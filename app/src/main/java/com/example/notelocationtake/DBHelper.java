9cbfae5293bea27ce279f20a3d9a497502c94694
Zak T Smith <32104151+Kaosorer@users.noreply.github.com>
2020-02-09 06:08:05 +0800

    dbhelper class made

---
+++ app/src/main/java/com/example/notelocationtake/DBHelper.java
@@ -0,0 +1,31 @@
+package com.example.notelocationtake;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "Notes.db";
+    public static final String TABLE_NAME = "NotesTable";
+    public static final String ID = "ID";
+    public static final String Title = "Title";
+    public static final String Content = "Content";
+    public static final String Address = "Address";
+
+    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
