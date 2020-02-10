df0ab4294abe41ab759f7b2d9ee17680f6ab6156
Zak T Smith <32104151+Kaosorer@users.noreply.github.com>
2020-02-11 06:21:58 +0800

    Activity Changes possible

--- app/src/main/java/com/example/notelocationtake/MainActivity.java
+++ app/src/main/java/com/example/notelocationtake/MainActivity.java
@@ -3,16 +3,39 @@ package com.example.notelocationtake;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         recyclerView = findViewById(R.id.noteRecyclerView);
+
+        FloatingActionButton addNoteFAB = findViewById(R.id.addNewNoteFAB);
+        addNoteFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Toast.makeText(MainActivity.this,"Button Clicked", Toast.LENGTH_SHORT).show();//Test FAB functionality
+
+                startActivity(new Intent(MainActivity.this, AddNote.class));
+
+            }
+        });
     }
+
+
+
 }
